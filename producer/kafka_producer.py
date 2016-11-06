0f98e0759f54f183db2defb89a1fc2589eeef7b3
Yiannis Sakkaris <yiannissakk@users.noreply.github.com>
2016-11-05 20:43:12 -0400

    Update kafka_producer.py

--- producer/kafka_producer.py
+++ producer/kafka_producer.py
@@ -19,34 +19,30 @@ class Producer(object):
         opener = urllib.URLopener()
         myurl = "https://s3-us-west-2.amazonaws.com/timo-twitter-data/2016-02-08-11-57_tweets.txt"
         myfile = opener.open(myurl)
-        k=0
-        
         
         printable = set(string.printable) #set of ascii (printable characters)
-        while True:
-            for line in myfile:
-                try:
-                    #filter tweets: keep fields of interest
-                    data = json.loads(line)
-                    userid_field = data['user']['id']
-                    username_field = data['user']['screen_name']
-                    text_field_a = (data['text']).encode('utf-8')
-                    text_field = filter(lambda x: x in printable, text_field_a) #remove any non ascii text
-                    createdat_field = data['created_at']
-                    tweetid_field = data['id']
-                    str_fmt = "{};{};{};{};{};{}"
-                    message_info = str_fmt.format(source_symbol,
-                                                  userid_field,
-                                                  text_field,
-                                                  createdat_field,
-                                                  tweetid_field,
-                                                  username_field)
+        for line in myfile:
+            try:
+                #filter tweets: keep fields of interest
+                data = json.loads(line)
+                userid_field = data['user']['id'
+                username_field = data['user']['screen_name']
+                text_field_a = (data['text']).encode('utf-8')
+                text_field = filter(lambda x: x in printable, text_field_a) #remove any non ascii text
+                createdat_field = data['created_at']
+                tweetid_field = data['id']
+                str_fmt = "{};{};{};{};{};{}"
+                message_info = str_fmt.format(source_symbol,
+                                              userid_field,
+                                              text_field,
+                                              createdat_field,
+                                              tweetid_field,
+                                              username_field)
                      
-                    #the producers queue messages in the form (topic, producer#, message)   
-                    self.producer.send_messages('tweets1', source_symbol, message_info)
-                    k=k+1
-                except:
-                    pass
+                #the producers queue messages in the form (topic, producer#, message)   
+                self.producer.send_messages('tweets1', source_symbol, message_info)
+             except:
+                pass
 
 if __name__ == "__main__":
     args = sys.argv
